e5b3fe3846157693220efbbf3293577233f2f7f8
ilbcj <ilbcj@hotmail.com>
2018-03-15 17:47:05 +0800

    chap 21 finish

--- app/src/main/java/com/anydef/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/anydef/android/beatbox/BeatBoxFragment.java
@@ -29,7 +29,8 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setRetainInstance(true);
+        
         mBeatBox = new BeatBox(getActivity());
     }
 
@@ -41,6 +42,12 @@ public class BeatBoxFragment extends Fragment {
         binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder {
